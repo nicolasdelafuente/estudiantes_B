b4de540bafd6e5deb17de424f1e07cadb7c98c38
nicolas.delafuente <ni.delafuente@gmail.com>
2022-01-04 13:11:28 -0300

    Seeders creadas

--- database/seeders/CarreraSeeder.php
+++ database/seeders/CarreraSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Carrera;
 use Illuminate\Database\Seeder;
 
 class CarreraSeeder extends Seeder
@@ -13,6 +14,28 @@ class CarreraSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Carrera::create([
+            'carrera_nombre' => "Licenciatura en Biotecnología"
+        ]);
+
+        Carrera::create([
+            'carrera_nombre' => "Licenciatura en Gestión Ambiental"
+        ]);
+
+        Carrera::create([
+            'carrera_nombre' => "Licenciatura en Tecnología de los Alimentos"
+        ]);
+
+        Carrera::create([
+            'carrera_nombre' => "Tecnicatura Universitaria en Tecnología de los Alimentos"
+        ]);
+
+        Carrera::create([
+            'carrera_nombre' => "Tecnicatura Universitaria en Gestión Ambiental"
+        ]);
+
+        Carrera::create([
+            'carrera_nombre' => "Tecnicatura Universitaria en Viverismo"
+        ]);
     }
 }
--- database/seeders/EstudianteSeeder.php
+++ database/seeders/EstudianteSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Estudiante;
 use Illuminate\Database\Seeder;
 
 class EstudianteSeeder extends Seeder
@@ -13,6 +14,40 @@ class EstudianteSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Estudiante::create([
+            'estudiante_nombre' => "Nombre 1",
+            'estudiante_apellido' => "Apellido 1",
+            'estudiante_carrera' => "1",
+        ]);
+
+        Estudiante::create([
+            'estudiante_nombre' => "Nombre 2",
+            'estudiante_apellido' => "Apellido 2",
+            'estudiante_carrera' => "2",
+        ]);
+
+        Estudiante::create([
+            'estudiante_nombre' => "Nombre 3",
+            'estudiante_apellido' => "Apellido 3",
+            'estudiante_carrera' => "3",
+        ]);
+        
+        Estudiante::create([
+            'estudiante_nombre' => "Nombre 4",
+            'estudiante_apellido' => "Apellido 4",
+            'estudiante_carrera' => "4",
+        ]);
+        
+        Estudiante::create([
+            'estudiante_nombre' => "Nombre 5",
+            'estudiante_apellido' => "Apellido 5",
+            'estudiante_carrera' => "5",
+        ]);
+        
+        Estudiante::create([
+            'estudiante_nombre' => "Nombre 6",
+            'estudiante_apellido' => "Apellido 6",
+            'estudiante_carrera' => "1",
+        ]);
     }
 }
